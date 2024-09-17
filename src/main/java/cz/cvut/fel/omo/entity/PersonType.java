cfe62886d1a7c9d37af96c92e97a476282b680bd
Jakub Oliberius <olibejak@fel.cvut.cz>
2024-09-17 15:14:39 +0200

    Entity structure

--- src/main/java/cz/cvut/fel/omo/entity/PersonType.java
+++ src/main/java/cz/cvut/fel/omo/entity/PersonType.java
@@ -1,4 +1,10 @@
 package cz.cvut.fel.omo.entity;
 
 public enum PersonType {
+    MOM,
+    DAD,
+    SON,
+    DAUGHTER,
+    GRANDMA,
+    GRANDPA,
 }
--- src/main/java/cz/cvut/fel/omo/entity/Pet.java
+++ src/main/java/cz/cvut/fel/omo/entity/Pet.java
@@ -1,4 +1,11 @@
 package cz.cvut.fel.omo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class Pet extends Entity {
+
+    private PetType type;
 }

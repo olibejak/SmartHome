528d13cb44ae85c305f0cec42d2fdf409f9630ab
Jakub Oliberius <olibejak@fel.cvut.cz>
2025-01-06 19:37:51 +0100

    Event factory trial

--- src/main/java/cz/cvut/fel/omo/BobTheBuilder/eventFactory/EventFactory.java
+++ src/main/java/cz/cvut/fel/omo/BobTheBuilder/eventFactory/EventFactory.java
@@ -1,8 +1,9 @@
 package cz.cvut.fel.omo.BobTheBuilder.eventFactory;
 
 import cz.cvut.fel.omo.event.Event;
+import cz.cvut.fel.omo.event.util.Payload;
 
 public interface EventFactory {
 
-    Event createEvent();
+    Event createEvent(Payload payload);
 }

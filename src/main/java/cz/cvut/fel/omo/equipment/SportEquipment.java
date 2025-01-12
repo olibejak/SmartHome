04e5d714f081bb0270e0413832a21a03f48da4f0
pazdela1 <pazdela1@fel.cvut.cz>
2025-01-12 17:46:47 +0100

    activity setup fix

--- src/main/java/cz/cvut/fel/omo/equipment/SportEquipment.java
+++ src/main/java/cz/cvut/fel/omo/equipment/SportEquipment.java
@@ -11,10 +11,5 @@ public abstract class SportEquipment {
     protected boolean isAvailable;
     protected int usageCount;
 
-    public SportEquipment() {
-        this.isAvailable = true;
-        this.usageCount = 0;
-    }
-
     public abstract String accept(SportEquipmentVisitor visitor);
 }

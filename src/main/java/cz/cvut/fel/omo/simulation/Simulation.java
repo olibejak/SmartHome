fa761b222341570448505442ac9f260bd8814f7a
pazdela1 <pazdela1@fel.cvut.cz>
2025-01-14 23:07:32 +0100

    simulation class constructors, logger

--- src/main/java/cz/cvut/fel/omo/simulation/Simulation.java
+++ src/main/java/cz/cvut/fel/omo/simulation/Simulation.java
@@ -5,6 +5,7 @@ import cz.cvut.fel.omo.activity.vehicle.Vehicle;
 import cz.cvut.fel.omo.entity.person.Person;
 import cz.cvut.fel.omo.entity.pet.Pet;
 import cz.cvut.fel.omo.house.House;
+import cz.cvut.fel.omo.logger.GlobalLogger;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,17 +16,39 @@ import java.util.ArrayList;
 @Getter
 @Setter
 public class Simulation {
+    protected GlobalLogger logger;
+
     private House house;
     private ArrayList<Person> family;
     private ArrayList<Pet> pets;
     private ArrayList<Vehicle> vehicles;
     private ArrayList<SportEquipment> equipment;
 
-    private int cycleCount = 0;
+    private int cycleCount;
+
+    public Simulation(House house, ArrayList<Person> family, ArrayList<Pet> pets, ArrayList<Vehicle> vehicles, ArrayList<SportEquipment> equipment) {
+        this.house = house;
+        this.family = family;
+        this.pets = pets;
+        this.vehicles = vehicles;
+        this.equipment = equipment;
+        this.cycleCount = 0;
+        this.logger = GlobalLogger.getInstance();
+    }
+
+    public Simulation(House house, ArrayList<Person> family, ArrayList<Pet> pets, ArrayList<Vehicle> vehicles, ArrayList<SportEquipment> equipment, int cycleCount) {
+        this.house = house;
+        this.family = family;
+        this.pets = pets;
+        this.vehicles = vehicles;
+        this.equipment = equipment;
+        this.cycleCount = cycleCount;
+        this.logger = GlobalLogger.getInstance();
+    }
 
     public void nextCycle() {
         cycleCount++;
-        System.out.println(cycleCount);
+        logger.info("Current cycle:" + cycleCount);
     }
 
     public void nextCycles(int count) {

8f14539131a9dd6acf6519e83d09639da4d56940
pazdela1 <pazdela1@fel.cvut.cz>
2025-01-14 23:57:17 +0100

    next cycle method structure

--- src/main/java/cz/cvut/fel/omo/simulation/Simulation.java
+++ src/main/java/cz/cvut/fel/omo/simulation/Simulation.java
@@ -1,7 +1,5 @@
 package cz.cvut.fel.omo.simulation;
 
-import cz.cvut.fel.omo.activity.equipment.SportEquipment;
-import cz.cvut.fel.omo.activity.vehicle.Vehicle;
 import cz.cvut.fel.omo.entity.person.Person;
 import cz.cvut.fel.omo.entity.pet.Pet;
 import cz.cvut.fel.omo.house.House;
@@ -21,6 +19,8 @@ public class Simulation {
     private House house;
     private ArrayList<Person> family;
     private ArrayList<Pet> pets;
+
+    // todo add eventQueue ??
 
     private int cycleCount;
 
@@ -43,6 +43,21 @@ public class Simulation {
     public void nextCycle() {
         cycleCount++;
         logger.info("Current cycle:" + cycleCount);
+
+        // 1. family and pets react to global events
+
+        // 2. family and pets actions
+        // - for each person and pet - two loops
+        //   2.1. find what people, pets, equipment, vehicles and events are in the current room
+        //   - special class CurrentRoomPayload with all of the above?? - findCurrentRoomPayloadByRoomID()
+        //   2.2. react to local events from CurrentRoomPayload
+        //   2.3. interact with people and pets from CurrentRoomPayload - with just one or all ??
+        //   2.4. interact with sport equipment, vehicles or devices from CurrentRoomPayload - probably with just one
+        //   2.5. make all equipment and vehicles available again
+        // 3. device actions
+        //   3.1. increase consumption based on the current state
+        // 4. family and pets movement
+        // - random choice?? probability tables??
     }
 
     public void nextCycles(int count) {

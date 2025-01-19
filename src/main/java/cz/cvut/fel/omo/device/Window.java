865afc09499055759c395619da0fb5990c4a345f
pazdela1 <pazdela1@fel.cvut.cz>
2025-01-19 15:42:41 +0100

    window toString tweak

--- src/main/java/cz/cvut/fel/omo/device/Window.java
+++ src/main/java/cz/cvut/fel/omo/device/Window.java
@@ -21,33 +21,33 @@ public class Window extends Device {
     public void open() {
         if (!isOpen) {
             isOpen = true;
-            logger.info(this.toString() + " window is opened");
+            logger.info(this + " window is opened");
         }
-        logger.info(this.toString() + " window is already opened");
+        logger.info(this + " window is already opened");
     }
 
     public void close() {
         if (isOpen) {
             isOpen = false;
-            logger.info(this.toString() + " window is closed");
+            logger.info(this + " window is closed");
         }
-        logger.info(this.toString() + " window is already closed");
+        logger.info(this + " window is already closed");
     }
 
     public void openCurtain() {
         if (!hasOpenedCurtain) {
             hasOpenedCurtain = true;
-            logger.info(this.toString() + " curtain is opened");
+            logger.info(this + " curtain is opened");
         }
-        logger.info(this.toString() + " curtain is already opened");
+        logger.info(this + " curtain is already opened");
     }
 
     public void closeCurtain() {
         if (hasOpenedCurtain) {
             hasOpenedCurtain = false;
-            logger.info(this.toString() + " curtain is closed");
+            logger.info(this + " curtain is closed");
         }
-        logger.info(this.toString() + " curtain is already closed");
+        logger.info(this + " curtain is already closed");
     }
 
     @Override

58af9eae5b06c88efcef551dcf36ef7c80b81b1a
Jakub Oliberius <olibejak@fel.cvut.cz>
2025-01-25 01:17:37 +0100

    Help command controls string update
    Signed-off-by: Jakub Oliberius <olibejak@fel.cvut.cz>

--- src/main/java/cz/cvut/fel/omo/simulation/command/HelpCommand.java
+++ src/main/java/cz/cvut/fel/omo/simulation/command/HelpCommand.java
@@ -4,15 +4,16 @@ public class HelpCommand implements Command {
 
     private static final String controls =
             """
-                A - House Configuration
-                S - Consumption Report
-                D - Next Cycle
-                H - Help
-                Q - Quit
+                Options:
+                  A - House Configuration
+                  S - Consumption Report
+                  D - Next Cycle
+                  H - Help
+                  Q - Quit
                 Usage:
-                A [F, R] [ID] - F for floor, R for room, ID for floor or room ID
-                S [F, R] [ID] - F for floor, R for room, ID for floor or room ID
-                D [number] - Number of cycles
+                  A [F, R] [ID] - F for floor, R for room, ID for floor or room ID
+                  S [F, R] [ID] - F for floor, R for room, ID for floor or room ID
+                  D [number] - Number of cycles
                 """;
 
     @Override

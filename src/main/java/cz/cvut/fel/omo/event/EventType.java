6364acc72c9c342c0874de46cda9fa8e323d926b
pazdela1 <pazdela1@fel.cvut.cz>
2025-01-26 00:16:29 +0100

    EventType refactoring

--- src/main/java/cz/cvut/fel/omo/event/EventType.java
+++ src/main/java/cz/cvut/fel/omo/event/EventType.java
@@ -2,10 +2,10 @@ package cz.cvut.fel.omo.event;
 
 public enum EventType {
 
-    DEVICE_MALFUNCTION,
-    RAIN,
-    DEVICE_JOB_DONE,
     DEVICE_EMPTY,
     DEVICE_FULL,
+    DEVICE_FINISHED,
+    DEVICE_BROKEN,
     TEMPERATURE_CHANGE,
+    RAIN
 }
